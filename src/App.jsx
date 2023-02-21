e5260794762a69eac1e27b3ff02186305b69a181
adalberto1996 <adalbertopedroza6@gmail.com>
2023-02-21 16:12:36 -0500

    implemetancion del localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,25 @@ import { checkWinnerFrom, checkendGame } from './board'
 import { WinnerModal } from './components/winnerModal'
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
-  const [turn, setTurn] = useState(TURN.X)
-  const [winner, setWinner] =  useState(null)
 
+  const [board, setBoard] = useState(() =>{ 
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURN.X
+  })
+  
+    const [winner, setWinner] =  useState(null)
 
 
   const resetGame= () => {
     setBoard(Array(9).fill(null))
     setTurn(TURN.X)
     setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
  
@@ -31,6 +38,9 @@ function App() {
 
     const newTurn = turn === TURN.X ? TURN.O : TURN.X
     setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    
     const newWinner = checkWinnerFrom(newBoard)
     if(newWinner){
       confetti()
@@ -43,6 +53,7 @@ function App() {
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
       <section className='game'>
           {
             board.map((_, index ) => {
